7e1e2bb04937028895cc08fa9aebb98f119226d3
BracketProto <creepertruck812@gmail.com>
2024-06-14 03:18:45 +0100

    changes

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Main() {
     <div id="root">
       <div id="background"></div>
       {/* Header */}
-      <div className="bg-black p-10 bg-opacity-75 font-bold text-cyan-500 text-3xl">
+      <div className="backdrop-blur bg-black p-10 bg-opacity-75 font-bold text-cyan-500 text-3xl">
         <h1>Portfolio</h1>
       </div>
 
@@ -17,7 +17,35 @@ export default function Main() {
               "I'm a software developer in the UK that uses the Unity game engine,",
               "Node JS, and some web development projects in HTML and CSS."],
             "text-cyan-500",
-            "bg-black bg-opacity-75"
+            "bg-black backdrop-blur bg-opacity-75"
+          )}
+
+        {Section
+          ("Programming Languages",
+            [
+              (<div><strong>Language</strong><strong className="fixed right-5">Confidence</strong></div>),
+              "\n\n",
+              LanguageElement("Javascript", "NodeJS, Browser", 100),
+              LanguageElement("C#", "Unity, Console Applications", 100),
+              LanguageElement("HTML", "Browser", 100),
+              LanguageElement("CSS", "Browser", 100),
+              LanguageElement("Python", "Default", 90),
+              LanguageElement("Typescript", "NodeJS", 60),
+              LanguageElement("C++", "Console", 50),
+              "\n\n\n\n",
+              Section
+                ("Libraries / Frameworks",
+                  ["*A list of some of the libraries and frameworks i've used*",
+                    "\n",
+                    "*Javascript:*",
+                    "Ws + Socket.io", "Express", "ThreeJS", "Electron"],
+                  "text-purple-500",
+                  "bg-black"
+                )
+            ],
+
+            "text-purple-500",
+            "bg-black backdrop-blur bg-opacity-75"
           )}
 
         {/* Projects */}
@@ -44,10 +72,10 @@ export default function Main() {
               "/images/voxel.png"
             ],
             "text-red-500",
-            "bg-black bg-opacity-75"
+            "bg-black backdrop-blur bg-opacity-75"
           )}
 
-        
+
         {/* Contact */}
         {Section("Contact Information",
           ["*Email*: connor.macdonald.791@accesscreative.ac.uk",
@@ -59,14 +87,43 @@ export default function Main() {
             "*Phone*: 07986015717"
           ],
           "text-green-500",
-          "bg-black bg-opacity-75"
+          "bg-black backdrop-blur bg-opacity-75"
         )}
       </div>
+
+      <br></br>
     </div>
   );
 }
 
-function Section(Title: string, Content: string[], TitleColor: string, BgColor: string) {
+function LanguageElement(Name: string, Content: string, Amount: number) {
+
+  var BarColor: string = "bg-black";
+  if (Amount > 90) {
+    BarColor = "bg-cyan-500";
+  }
+  else if (Amount > 75) {
+    BarColor = "bg-green-500";
+  }
+  else if (Amount > 50) {
+    BarColor = "bg-yellow-500";
+  }
+  else if (Amount >= 0) {
+    BarColor = "bg-red-500";
+  }
+
+  return (
+    <div className="w-full h-full">
+      <strong className="inline-block">{Name} &nbsp;&nbsp;&nbsp;</strong>
+      <div className="absolute mr-5 inline-block w-9/12">
+        <div style={{ width: "calc(" + Amount / 2 + "% - 5vh)", height: "25px" }} className={"languagebar " + BarColor + " p-[.1vw] truncate fixed right-5 rounded-[2.5rem]"}><p className="relative bottom-0 left-5">{Content}</p></div>
+      </div>
+      <br /><br />
+    </div>
+  );
+}
+
+function Section(Title: string, Content: any[], TitleColor: string, BgColor: string) {
 
   const ProcessContent = (Text: string) => {
     return Text.split('\n').map((Line, LineIndex) => (
@@ -107,13 +164,13 @@ function Section(Title: string, Content: string[], TitleColor: string, BgColor: 
 
   if (Title !== "") {
     FinalContent = (
-      <div>
+      <div className="w-full">
         <h1 className={`${TitleColor} text-3xl`}>{Title}</h1>
         <br />
         {Content.map((Item, Index) => (
-          <p key={Index}>
-            {ProcessContent(Item)}
-          </p>
+          <div key={Index}>
+            {typeof Item === 'string' ? ProcessContent(Item) : Item}
+          </div>
         ))}
       </div>
     );
@@ -121,17 +178,20 @@ function Section(Title: string, Content: string[], TitleColor: string, BgColor: 
     FinalContent = (
       <div>
         {Content.map((Item, Index) => (
-          <p key={Index}>
-            {ProcessContent(Item)}
-          </p>
+          <div className="w-full" key={Index}>
+            {typeof Item === 'string' ? ProcessContent(Item) : Item}
+          </div>
         ))}
       </div>
     );
   }
 
   return (
-    <div className={`p-10 m-10 rounded-lg max-w-screen-md ${BgColor}`}>
-      {FinalContent}
+    <div>
+      <div className={`p-10 m-border rounded-[2.5rem] max-w-screen-md ${BgColor}`}>
+        {FinalContent}
+      </div>
+      <br></br>
     </div>
   );
 }
