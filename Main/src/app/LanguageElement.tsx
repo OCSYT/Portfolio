e7faa7a616888e4b71befabc74e54b963e9fbf1a
BracketProto <69521768+OCSYT@users.noreply.github.com>
2024-10-07 16:07:09 +0100

    Mobile site working

--- Main/src/app/LanguageElement.tsx
+++ Main/src/app/LanguageElement.tsx
@@ -19,7 +19,7 @@ function LanguageElement(Name: string, Content: string, Amount: number) {
         <div className="w-full h-full">
             <strong className="inline-block">{Name} &nbsp;&nbsp;&nbsp;</strong>
             <div className="absolute mr-5 inline-block w-9/12">
-                <div style={{ width: "calc(" + Amount / 2 + "% - 2vh)", height: "25px" }} className={"languagebar " + BarColor + " p-[.1vw] truncate fixed right-5 rounded-[2.5rem]"}><p className="relative bottom-0 left-5">{Content}</p></div>
+                <div style={{ width: "calc(" + Amount / 2 + "% - 2vh)", height: "25px" }} className={"languagebar " + BarColor + " p-[.1vw] truncate fixed right-5 rounded-[2.5rem]"}><p className="relative language-text left-5">{Content}</p></div>
             </div>
             <br /><br />
         </div>
--- Main/src/app/page.tsx
+++ Main/src/app/page.tsx
@@ -10,17 +10,20 @@ import ContactInfo from './Contact';
 export default function Main() {
   return (
     <div id="root">
+      <div id="background" />
       <CreateRain />
-      <div className="m-0 p-0 w-full flex flex-col min-h-screen justify-center items-center container">
-        {/* Header */}
-        <div className="bg-transparent p-10 font-bold text-cyan-500 text-3xl mb-10 top-0 z-10 text-center">
-          <h1>Connor Macdonald</h1>
+      <div className="mr-10 p-0 w-full flex justify-center items-center">
+        <div className="flex flex-col justify-center items-center">
+          {/* Header */}
+          <div className="title bg-transparent p-10 font-bold text-cyan-500 text-3xl mb-10 top-0 z-10 text-center">
+            <h1>Connor Macdonald</h1>
+          </div>
+          <Introduction></Introduction>
+          <ProgrammingLanguages></ProgrammingLanguages>
+          <LibrariesAndFrameworks></LibrariesAndFrameworks>
+          <Projects></Projects>
+          <ContactInfo></ContactInfo>
         </div>
-        <Introduction></Introduction>
-        <ProgrammingLanguages></ProgrammingLanguages>
-        <LibrariesAndFrameworks></LibrariesAndFrameworks>
-        <Projects></Projects>
-        <ContactInfo></ContactInfo>
       </div>
     </div>
   );
